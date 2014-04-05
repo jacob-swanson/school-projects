d79932905b1980f6693cf3f70df4d34cd99c375c
Jacob Swanson <Jacob.Swanson.K87@Gmail.com>
2014-04-05 17:50:08 -0500

    WaveFilter: Updated filter.

--- Signals/WaveFilter/AnalyzeFrequency.m
+++ Signals/WaveFilter/AnalyzeFrequency.m
@@ -1,9 +1,14 @@
-[y,Fs] = wavread('Swanson_J_mod.wav');
-length = length(y);
+% Adapted from http://www.mathworks.com/help/matlab/ref/fft.html
 
-% Take transform
-Y = fft(y);
-Y = Y(1:length/2+1);
-freq = 0:Fs/length:Fs/2;
-plot(freq,Y);
-xlabel('Hz');
+[y,Fs] = wavread('Swanson_J_mod.wav'); % Wav plus noise
+T = 1/Fs;		% Sample time
+L = length(y);	% Length of signal
+t = (0:L-1)*T;	% Time vector
+
+NFFT = 2^nextpow2(L); % Next power of 2 from length of y
+Y = fft(y,NFFT)/L;
+f = Fs/2*linspace(0,1,NFFT/2+1);
+plot(f,2*abs(Y(1:NFFT/2+1))) 
+title('Single-Sided Amplitude Spectrum of y(t)')
+xlabel('Frequency (Hz)')
+ylabel('|Y(f)|')
